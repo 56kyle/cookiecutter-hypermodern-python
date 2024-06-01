8c94447b874af6d5ed00c30d5f8f817fa0ecdb26
Kyle Oliver <oliverke@mail.uc.edu>
2024-06-01 14:17:45 -0400

    Adds first time setup.

--- {{cookiecutter.project_name}}/scripts/first_time_setup.bat
+++ {{cookiecutter.project_name}}/scripts/first_time_setup.bat
@@ -1,7 +1,9 @@
-:: Must set TWINE_USERNAME and TWINE_PASSWORD prior to running
+:: Make sure to create the repo prior to running
 git init
 git add .
 git commit -m "Initial commit."
-
+git branch -M main
+git remote add origin https://github.com/56kyle/{{cookiecutter.project_name}}.git
+git push -u origin main
 poetry install --with dev
 poetry build
