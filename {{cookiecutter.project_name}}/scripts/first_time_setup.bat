d8cef129d67dbf396a1375d41f1fee22181977c7
Kyle Oliver <oliverke@mail.uc.edu>
2024-06-01 21:16:04 -0400

    Replaces some pycharm generated commands with more generic git CLI usages.

--- {{cookiecutter.project_name}}/scripts/first_time_setup.bat
+++ {{cookiecutter.project_name}}/scripts/first_time_setup.bat
@@ -8,10 +8,6 @@ git branch -M main
 git remote add origin https://github.com/56kyle/{{cookiecutter.project_name}}.git
 git push -u origin main
 
-:: Create develop branch
-git -c credential.helper= -c core.quotepath=false -c log.showSignature=false checkout -b develop main^0 --
-git push -u origin develop
-
 :: Install poetry
 poetry env use %PYTHON39%
 poetry install --with dev
@@ -23,13 +19,18 @@ nox -s pre-commit -- install
 :: Update poetry.lock
 poetry update
 git add .
-git commit -m "Runs poetry update."
+git commit --message="Runs poetry update."
 git push
 
 :: Creates first release
-git -c credential.helper= -c core.quotepath=false -c log.showSignature=false checkout -b release/0.0.1 develop^0 --
+git switch --create release main
 poetry version patch
-git add .
-git commit -m "Bumps version to 0.0.1."
-git push -u origin release/0.0.1
+git commit --message="{{cookiecutter.project_name}} 0.0.1" pyproject.toml
+git push origin release
 
+:: Switch back to main
+git checkout main
+
+:: Create develop branch
+git switch -c develop
+git push -u origin develop
