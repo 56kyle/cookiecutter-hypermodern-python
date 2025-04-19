bf906d0a2bc5eb16fa3ac68ec4ca1acd01d33687
koliver_kv <koliver@saec-kv.com>
2025-04-19 03:30:28 -0400

    chore(noxfile): replace coverage[toml] installation with coverage

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -169,7 +169,7 @@ def coverage(session: Session) -> None:
     """Produce the coverage report."""
     args = session.posargs or ["report"]
 
-    session.install("coverage[toml]")
+    session.install("coverage")
 
     if not session.posargs and any(Path().glob(".coverage.*")):
         session.run("coverage", "combine")
